d01eb441feae5efe4470bf6ad599628b028da645
Elena <92665632+shmetanelena@users.noreply.github.com>
2022-05-21 20:16:50 +0300

    complete all tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const input = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener(
+  'input',
+  debounce(e => {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+
+    const name = input.value.trim();
+    if (name.length === 0) {
+      return;
+    }
+
+    fetchCountries(name)
+      .then(countries => {
+        if (countries.length > 10) {
+          Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (countries.length > 1 && countries.length <= 10) {
+          countryList.innerHTML = countries
+            .map(
+              country => `
+            <li class="country-list-item">               
+                <img class="flag" src=${country.flags.svg} alt="${country.name.official}"/>
+                ${country.name.official}            
+            </li>`
+            )
+            .join('');
+        } else if (countries.length == 1) {
+          const country = countries[0];
+
+          countryInfo.innerHTML = `
+                <div class="country-header">
+                    <img class="flag" src=${country.flags.svg} alt="${country.name.official}"/>
+                    <h1 class="country-name">${country.name.official}</h1>
+                </div>
+                <ul>
+                    <li class="country-list-item">
+                        <span class="country-attibute">Capital:</span>
+                        ${country.capital[0]}
+                    </li>
+                    <li class="country-list-item">
+                        <span class="country-attibute">Population:</span>
+                        ${country.population}</span>
+                    </li>
+                    <li class="country-list-item">
+                        <span class="country-attibute">Languages:</span>
+                        ${Object.values(country.languages).join(', ')}
+                    </li>
+                </ul>`;
+        }
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      });
+  }, DEBOUNCE_DELAY)
+);
